aa784786739c771b11621d169ce453008aba1adb
Qianglong Mo <sammok2003@hotmail.com>
2015-01-05 02:23:17 +0800

    Add scrollJump for nav, Add some little animations.

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,16 @@
 /* Qianglong Mo 2014 (Sumi Interactive) */
+
+$(function (){
+    $(window).stellar();
+
+    // jump to target
+    $('.target1').click(function (){
+        $('html,body').animate({scrollTop:$('.section-01').offset().top - 18}, 800);
+    });
+    $('.target2').click(function (){
+        $('html,body').animate({scrollTop:$('.section-02').offset().top - 18}, 800);
+    });
+    $('.target3').click(function (){
+        $('html,body').animate({scrollTop:$('.section-03').offset().top - 18}, 800);
+    });
+});
