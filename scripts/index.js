24acb5edc2260e8e959ca1e129261110777e9057
Qianglong Mo <sammok2003@hotmail.com>
2015-04-01 10:31:40 +0800

    Add animation wow plugin to  page.

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,6 @@
 /* Qianglong Mo 2014 (Sumi Interactive) */
 
 $(function (){
-    $(window).stellar();
-
     // jump to target
     $('.target1').click(function (){
         jumpToTarget('.section-01', '.section-title-01');
@@ -16,13 +14,10 @@ $(function (){
 
     function jumpToTarget(section, sectionTitle){
         $('html,body').animate({scrollTop:$(section).offset().top - 18}, 800);
-
-        setTimeout(function (){
-            $(sectionTitle).addClass('animated bounceInLeft');
-            setTimeout(function (){
-                $(sectionTitle).removeClass('animated bounceInLeft');
-            }, 1000);
-        }, 400);
     }
 
+    //  wow scroll animation
+    new WOW({
+        offset: 20
+    }).init();
 });
